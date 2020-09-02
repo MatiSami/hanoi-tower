a8b1ce6ef3f4c7de47238c12e7935dd358cc0c72
Mateusz Samula <msamula@comperia.pl>
2020-09-02 15:31:55 +0200

    add newnew functionalities have been added

--- app.js
+++ app.js
@@ -19,18 +19,58 @@ const newGameBox = document.getElementById("new-game-box")
 const leftButton = document.getElementById("left-button")
 const rightButton = document.getElementById("right-button")
 const userChoice = document.getElementById("user-choice")
+const userNameWinMessage = document.querySelector(".winner-meesage-user-name")
+const userResultList = document.querySelector(".user-result--list")
+const userData = document.getElementById("user-data")
+const settingsIcon = document.getElementById("setting")
+const userResultBox = document.querySelector(".user-game-stats")
+const closeBox = document.querySelector(".close-box")
+const resultMessage = document.querySelector(".result-message")
+const userName = document.querySelector(".user-name")
+const settingBox = document.querySelector(".settings-box")
+const closeSettings = document.querySelector(".close-settings")
+const settingForm = document.getElementById("setting-form")
+
+console.log(userName);
 
 let time;
 let hanoiElements = 3;
 let moves = 0
 let timerIsOff = true
+let gameRulesModalIsOpen = true
+let gameHistoryModalIsOpen = false
+let gameSettingModalIsOpen = false
 let minutes = 0
 let seconds = 0
 let houres = 0
 let totalMovesNeeds = Math.pow(2, hanoiElements) - 1;
 let gameRulesOn = true
+let userNamePrompt
 
-let scoreBoard = []
+let newScore = {
+    userName: "",
+    userGameTime: "",
+    userTotalMove: "",
+    elementsToMove: ""
+}
+
+function getUserName() {
+    if (localStorage.name == "" || localStorage.name == undefined || localStorage.name == null) {
+        userNamePrompt = prompt("Podaj swoje imię");
+        if (userNamePrompt === null || userNamePrompt === "") {
+            userNamePrompt = "Nieznajomy"
+            userName.innerHTML = userNamePrompt
+        } else {
+            localStorage.setItem("name", userNamePrompt);
+            userName.innerHTML = userNamePrompt
+        }
+    } else {
+        userNamePrompt = localStorage.name
+        userName.innerHTML = userNamePrompt
+    }
+}
+
+getUserName()
 
 // wybór liczby elementów do ułożenia 
 Array.from(howManyElements).forEach(function (element) {
@@ -120,7 +160,6 @@ function generateHanoiElements() {
 
 function setDraggable(element) {
     Array.from(blocks).forEach(function (element) {
-        console.log(element.childNodes);
         let children = element.children
         if (element.childNodes.length > 0) {
             Array.from(children).forEach((element => {
@@ -135,10 +174,8 @@ function setDraggable(element) {
 }
 
 function endGame() {
-
-    // if (blockRight.childNodes.length === hanoiElements) {
-    console.log("wygrałeś");
     clearInterval(timerPlay)
+    userNameWinMessage.innerHTML = localStorage.name;
     if (moves > totalMovesNeeds) {
         extraText.innerHTML = "Bardzo dobrze, ale mółbyś to zrobić w " + totalMovesNeeds + " ruchach"
     } else {
@@ -150,7 +187,14 @@ function endGame() {
     leftButton.style.display = "none"
     rightButton.style.display = "none"
     userChoice.style.display = "none"
-    // }
+
+
+    newScore.userName = localStorage.name;
+    newScore.userGameTime = totalTime.innerHTML;
+    newScore.userTotalMove = totalMoves.innerHTML;
+    newScore.elementsToMove = String(hanoiElements)
+
+    saveScoreToLocalStorage(newScore)
 }
 
 
@@ -167,11 +211,9 @@ function timer() {
             if (seconds < 10) {
                 seconds = "0" + seconds
             }
-
             if (seconds == 60) {
                 seconds = 0
                 minutes++
-
                 if (minutes == 60) {
                     minutes = 0
                     houres++
@@ -183,33 +225,76 @@ function timer() {
 }
 
 function refresh() {
-    time = minutes + ":" +  seconds;
+    time = minutes + ":" + seconds;
     document.getElementById("timer").innerHTML = time;
 }
 
 function newGame() {
-
+    // czyszczenie starych elementów piramidy
     Array.from(blocks).forEach(function (element) {
         while (element.firstChild) {
             element.firstChild.remove()
         }
     })
+    // zerowanie licznika czasu
     if (!timerIsOff) {
         clearInterval(timerPlay)
         timerIsOff = true
         minutes = 0
-        seconds = 0
+        seconds = "0" + 0
         houres = 0
         refresh()
     }
+    // zerowanie licznika ruchów wykonanych przez uzytkownika
     moves = 0
     movesCounter.innerHTML = moves
+    // generowanie nowych elementów piramidy
     generateHanoiElements()
+    // ustawianie draggable na najwyżym lemencie bloku lewego
     setDraggable(blockLeft)
+    // wyliczanie minimalnej liczby ruchów do ułożenia wiezy
     totalMovesNeeds = Math.pow(2, hanoiElements) - 1;
     minMovesNeed.innerHTML = totalMovesNeeds
-    console.log(totalMovesNeeds);
+    // pokazanie bloku z mozliwoscia wyboru liczby elementow
     userChoice.style.display = "flex"
+}
+
+
+function saveScoreToLocalStorage(newObject) {
+    let dataFromLocalStorage = [];
+    if (localStorage.getItem("results") != null) {
+        dataFromLocalStorage = JSON.parse(localStorage.getItem("results"));
+        dataFromLocalStorage.push(newObject)
+        localStorage.setItem("results", JSON.stringify(dataFromLocalStorage))
+    } else {
+        dataFromLocalStorage.push(newObject)
+        localStorage.setItem("results", JSON.stringify(dataFromLocalStorage))
+    }
+}
+
+
+function renderAllResults() {
+    let allResults = JSON.parse(localStorage.getItem("results"))
+    if (allResults === null) {
+        console.log("musisz zagrać żeby pojawiły się wynik");
+        resultMessage.style.display = "block"
+    } else {
+        resultMessage.style.display = "none"
+        allResults.forEach(function (singleResult) {
+
+            if (singleResult.userName === localStorage.name) {
+                resultMessage.style.display = "none"
+                let markup = `
+        <div class="user-result-single-record">
+        <div>Gracz <span class="result-span">${singleResult.userName}</span></div>
+        <div>Liczba elementów do ułożenia <span class="result-span">${singleResult.elementsToMove}</span></div>
+        <div>Czas wykonania <span class="result-span">${singleResult.userGameTime}</span> </div>
+        <div>Liczba ruchów <span class="result-span">${singleResult.userTotalMove}</span></div>
+        </div>`;
+                userResultList.insertAdjacentHTML('beforeend', markup)
+            }
+        })
+    }
 }
 
 newGameButton.addEventListener("click", function () {
@@ -226,16 +311,20 @@ gameRulesButton.addEventListener("click", function () {
     gameRules.style.display = "none"
     leftButton.style.display = "block"
     rightButton.style.display = "block"
+    gameRulesModalIsOpen = false
     timer()
 })
 
 gameInstruction.addEventListener("click", function () {
+    if (!gameHistoryModalIsOpen && !gameSettingModalIsOpen) {
     gameRules.style.display = "flex";
     gameContainer.classList.add("supreme")
     leftButton.style.display = "none"
     rightButton.style.display = "none"
+    gameRulesModalIsOpen = true
     clearInterval(timerPlay)
     timerIsOff = true
+    }
 })
 
 newGameBox.addEventListener("click", function () {
@@ -243,4 +332,67 @@ newGameBox.addEventListener("click", function () {
 })
 
 
-alert
+userData.addEventListener("click", function () {
+    if (!gameSettingModalIsOpen && !gameRulesModalIsOpen) {
+    userResultBox.style.display = "flex"
+    gameContainer.classList.add("supreme")
+    leftButton.style.display = "none"
+    rightButton.style.display = "none"
+    renderAllResults()
+    clearInterval(timerPlay)
+    timerIsOff = true
+    gameHistoryModalIsOpen = true
+    }
+})
+
+
+settingsIcon.addEventListener('click', function () {
+    console.log(gameHistoryModalIsOpen);
+    console.log(gameRulesModalIsOpen);
+    if (!gameHistoryModalIsOpen && !gameRulesModalIsOpen) {
+        settingBox.style.display = "flex"
+        gameContainer.classList.add("supreme")
+        leftButton.style.display = "none"
+        rightButton.style.display = "none"
+        clearInterval(timerPlay)
+        timerIsOff = true
+        gameSettingModalIsOpen = true
+    }
+
+})
+
+
+closeBox.addEventListener("click", function () {
+    userResultBox.style.display = "none"
+    userResultList.innerHTML = ""
+    timer()
+    gameContainer.classList.remove("supreme")
+    leftButton.style.display = "block"
+    rightButton.style.display = "block"
+    gameHistoryModalIsOpen = false
+})
+
+closeSettings.addEventListener("click", function () {
+    settingBox.style.display = "none"
+    timer()
+    settingForm.reset()
+    gameContainer.classList.remove("supreme")
+    leftButton.style.display = "block"
+    rightButton.style.display = "block"
+    gameSettingModalIsOpen = false
+})
+
+settingForm.addEventListener("submit", function (e) {
+    e.preventDefault()
+    let newUserName = document.getElementById("name")
+    localStorage.name = newUserName.value
+    userName.innerHTML = localStorage.name
+    settingForm.reset()
+    settingBox.style.display = "none"
+    gameContainer.classList.remove("supreme")
+    leftButton.style.display = "block"
+    rightButton.style.display = "block"
+    timerIsOff = false
+    gameSettingModalIsOpen = false
+    newGame()
+})
